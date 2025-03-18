ae75efcbbbb0b0e929e5b29503eeb0c39000df53
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-03-18 23:14:06 +0100

    added new package : kitchen added new file: models.py that includes classes Cook, DishType and Dish

---
+++ kitchen/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.forms import models
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook)
+
+    def __str__(self):
+        return f"{self.name} ({self.dish_type}) ({self.description}) ({self.price})"
