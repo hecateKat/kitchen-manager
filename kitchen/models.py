dca4e82afdc6f9d3dda5435254333a688f221a13
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-03-20 16:54:39 +0100

    refactor: models.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -23,17 +23,16 @@ class DishType(models.Model):
 
 class Dish(models.Model):
     name = models.CharField(max_length=100)
-    ingredients = models.ManyToManyOne(Ingredient)
+    ingredients = models.ManyToManyField(Ingredient)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook)
 
     def __str__(self):
-        return f"{self.name} ({self.dish_type}) ({self.description}) ({self.price})"
+        return f"{self.name} ({self.dish_type})"
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, unique=True)
     quantity = models.CharField(max_length=50)
-    dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name='ingredients')
